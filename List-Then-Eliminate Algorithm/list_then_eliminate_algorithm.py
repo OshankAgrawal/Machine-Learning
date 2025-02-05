abbcaa8d761f07c011c7c72989517d19fd01d53e
OshankAgrawal <oshankagrawal@gmail.com>
2025-02-05 20:03:10 +0530

    List-then-Eliminate Algorithm

--- List-Then-Eliminate Algorithm/list_then_eliminate_algorithm.py
+++ List-Then-Eliminate Algorithm/list_then_eliminate_algorithm.py
@@ -1,23 +1,50 @@
 import numpy as np
 import pandas as pd
+from itertools import product
 
-def create_version_space(f1,f2):
-    f1.append('?')
-    f2.append('?')
+csv_file_data=pd.read_csv('Data.csv')
+#print(csv_file_data)
 
-    # Create Semantical Distinct Hypothesis Space
-    semantical_distinct=[]
-    for i in f1:
-        for j in f2:
-            semantical_distinct.append([i,j])
+def create_sementical_distinct_hypothesis(data):
+    # Extract the examples of the DataSet
+    examples=np.array(data)[:,:-1]
+    # print(examples)
 
-    new_item=['$']*2
-    semantical_distinct.append(new_item)
+    number_of_features=len(examples[0])
+    #print(number_of_features)
 
-    return semantical_distinct
+    # Transpose the examples so that it is easy to make the set of the values of feature
+    feature_transpose=np.transpose(examples)
+    # print(feature_transpose)
 
-def compare_feature_vector(arr1,arr2):
-     # Check the corresponding feature value is acceptable or not
+    # Declare a 2D Array which store all the features set
+    feature=[[]]*number_of_features
+    # print(feature)
+
+    # Iterations which make the sets of features value and also convert them into the 2D Array
+    for i in range(len(feature_transpose)):
+        my_set=set()
+        for j in range(len(feature_transpose[0])):
+            my_set.add(feature_transpose[i][j])
+        # print(my_set)
+        feature[i]=list(my_set)
+    # print(feature)
+
+    # Append the general condition in all the feature vectors
+    for i in range(len(feature)):
+        feature[i].append('?')
+    # print(feature)
+
+    # Create the Sementical Distinct Hypothesis
+    sementical_distinct_hypothesis=list(product(*feature))
+    # print(sementical_distinct_hypothesis)
+    # print(len(sementical_distinct_hypothesis))
+
+    # Return the Sementical Distinct Hypothesis
+    return sementical_distinct_hypothesis
+
+def compare_feature_Vector(arr1,arr2):
+    # Check the corresponding feature value is acceptable or not
     # if acceptable return True otherwise return False
     length=len(arr1)
     for i in range(length):
@@ -29,11 +56,11 @@ def compare_feature_vector(arr1,arr2):
             return False
     return True
 
-def check_hypothesis(hypothesis,data,target):
+def check_hypothesis(hypothesis,instance,target):
     # Check Hypothesis is consistent or not
     # If consistent return True otherwise return False
-    for i,val in enumerate(data):
-        if compare_feature_vector(hypothesis,val):
+    for i,val in enumerate(instance):
+        if compare_feature_Vector(hypothesis,val):
             if target[i]=='Yes':
                 pass
             else:
@@ -45,7 +72,15 @@ def check_hypothesis(hypothesis,data,target):
                 return False
     return True
 
-def eliminate_hypothesis(semantical_distinct,data,target):
+def create_version_space(csv_file_data):
+    semantical_distinct=create_sementical_distinct_hypothesis(csv_file_data)
+
+    data=np.array(csv_file_data)[:,:-1]
+    # print(data)
+
+    target=np.array(csv_file_data)[:,-1]
+    # print(target)
+
     # It appends all the consistent hypothesis to the Version Space
     version_space=[]
     for i in semantical_distinct:
@@ -53,23 +88,5 @@ def eliminate_hypothesis(semantical_distinct,data,target):
             version_space.append(i)
     return version_space
 
-
-f1=['A','B','C']
-f2=['X','Y','Z']
-version_space=create_version_space(f1,f2)
-# print("Version Space:- ",version_space)
-
-csv_file_data=pd.read_csv('Data.csv')
-#print(csv_file_data)
-
-data=np.array(csv_file_data)[:,:-1]
-#print(data)
-
-target=np.array(csv_file_data)[:,-1]
-#print(target)
-
-eliminated_version_space=eliminate_hypothesis(version_space,data,target)
-
-print(eliminated_version_space)
-
-
+version_space=create_version_space(csv_file_data)
+print(version_space)
